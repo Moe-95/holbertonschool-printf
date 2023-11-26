36705245838fc7e04ba2a963fc88556916e8555a
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-26 18:19:49 +0200

    2

--- main.h
+++ main.h
@@ -8,7 +8,7 @@ int _printf(const char *format, ...);
 int _strlen(char *s);
 int _write_formatted(const char *format, va_list args);
 int _write_int(int num);
-int _write_string(const char *str);
+int _write_string(char *str);
 int _write_char(char c);
 
 #endif
