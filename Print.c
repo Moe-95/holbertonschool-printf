b6e49eb7312a97d7ec8f7475418303ce55a43548
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-26 18:01:06 +0200

    1

--- Print.c
+++ Print.c
@@ -1,34 +1,46 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
-#include "main.h"
+#include <string.h>
 
 /**
-*write_char - Writes a character to standard output.
-*@c: char to write
-*Return: Int.
-*/
-int write_char(char c)
+ * _write_char - writes a character to stdout
+ * @c: character to write
+ * Return: number of characters written
+ */
+int _write_char(char c)
 {
 return write(1, &c, 1);
 }
 
 /**
-*write_string - Writes a string to standard output.
-*@str: String to write.
-*Return: Int.
+* _write_string - writes a string to stdout
+* @str: string to write
+* Return: number of characters written
 */
-int write_string(const char *str)
+int _write_string(char *str)
 {
-return write(1, str, strlen(str));
+if (str != NULL)
+{
+int len = write(1, str, strlen(str));
+if (len == -1)
+{
+return -1;
+}
+return len;
+}
+else
+{
+return write(1, "(null)", 6);
+}
 }
 
 /**
-*write_int - Writes an integer to standard output.
-*@num: Int to write.
-*Return: Int.
+* _write_int - writes an integer to stdout
+* @num: integer to write
+* Return: number of characters written
 */
-int write_int(int num)
+int _write_int(int num)
 {
 char buffer[50];
 int len = snprintf(buffer, sizeof(buffer), "%d", num);
@@ -36,14 +48,21 @@ return write(1, buffer, len);
 }
 
 /**
-*write_formatte - Writes a formatted string to standard output.
-*@format: String format.
-*@args: args.
-*Return: Int.
+* _printf - print formatted string.
+* @format: format to use.
+* Return: number of characters printed
 */
-int write_formatted(const char *format, va_list args)
+int _printf(const char *format, ...)
 {
 int count = 0;
+if (format == NULL)
+{
+return write(1, NULL, 1);
+}
+else
+{
+va_list args;
+va_start(args, format);
 while (*format != '\0')
 {
 if (*format == '%')
@@ -52,48 +71,32 @@ format++;
 switch (*format)
 {
 case 'c':
-count += write_char(va_arg(args, int));
+count += _write_char(va_arg(args, int));
 break;
 case 's':
-count += write_string(va_arg(args, char *));
+count += _write_string(va_arg(args, char *));
 break;
 case '%':
-count += write_char('%');
+count += _write_char('%');
 break;
 case 'd':
 case 'i':
-count += write_int(va_arg(args, int));
+count += _write_int(va_arg(args, int));
 break;
 case '\0':
+count = write(1, NULL, 1);
 return count;
 default:
-count += write_string(format - 1);
+count += write(1, format - 1, 2);
 }
 }
 else
 {
-count += write_char(*format);
+count += write(1, format, 1);
 }
 format++;
 }
-return count;
-}
-
-/**
-* _printf - print formatted string.
-* @format: format to use.
-* Return: Number of characters written (excluding null byte).
-*/
-int _printf(const char *format, ...)
-{
-if (format == NULL)
-{
-write_char(NULL);
-return 0;
-}
-va_list args;
-va_start(args, format);
-int count = write_formatted(format, args);
 va_end(args);
 return count;
+}
 }
--- main.h
+++ main.h
@@ -6,9 +6,9 @@
 /* Function prototype for _printf */
 int _printf(const char *format, ...);
 int _strlen(char *s);
-int write_formatted(const char *format, va_list args);
-int write_int(int num);
-int write_string(const char *str);
-int write_char(char c);
+int _write_formatted(const char *format, va_list args);
+int _write_int(int num);
+int _write_string(const char *str);
+int _write_char(char c);
 
 #endif
