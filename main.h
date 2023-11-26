f166eb5a7f1b0990c779bbfae11fb8f90a2a310e
Moe-95 <mhammad_fahs@hotmail.com>
2023-11-26 15:03:41 +0200

    first try.

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/* Function prototype for _printf */
+int _printf(const char *format, ...);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,64 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <stdio.h>
+#include <main.h>
+/**
+*_printf - print formatted string.
+*@format: format to use.
+*Return: Always 0.
+*/
+int _printf(const char *format, ...)
+{
+va_list args;
+va_start(args, format);
+int count = 0;
+char c;
+char *str;
+int num, len;
+char buffer[20];
+while (*format != '\0')
+{
+if (*format == '%') {
+format++;  // Move past the '%'
+switch (*format)
+{
+case 'c':
+c = va_arg(args,int);
+count += write(1, &c, 1);
+break;
+case 's':
+{
+*str = va_arg(args, char*);
+count += write(1, str, strlen(str));
+break;
+}
+case '%':
+count += write(1, "%", 1);
+break;
+case 'd':
+case 'i':
+{
+num = va_arg(args, int);
+len = snprintf(buffer, sizeof(buffer), "%d", num);
+count += write(1, buffer, len);
+break;
+}
+default:
+// Unsupported specifier, ignore it
+count += write(1, format - 1, 2); // Write the '%', and the character following '%'
+}
+}
+else
+{
+// Regular character, write it to stdout
+count += write(1, format, 1);
+}
+format++;  // Move to the next character in the format string
+}
+
+va_end(args);
+return count;
+}
